bb653bdde4151c379d702df381fbb331442291dd
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-04 17:07:15 +0200

    i try to fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,13 +4,11 @@ def fizzbuzz():
     for num in range(1,101):
         string = ""
         if num % 3 == 0:
-            string = string + "Fizz"
+            string += "Fizz"
         if num % 5 == 0:
-            string = string + "Buzz"
+            string += "Buzz"
         if num % 3 != 0 and num % 5 != 0:
-            string = string + str(num)
-        print((string), end=" ")
-
-    print("$")
+            string += str(num)
+        print(string, end=" ")
 
 fizzbuzz()
