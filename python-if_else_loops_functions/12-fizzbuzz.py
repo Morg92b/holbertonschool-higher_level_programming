3ec554962e1fcb3fe41eef0ff339d1f721634afb
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-04 16:58:18 +0200

    add fizzbuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+def fizzbuzz():
+    for num in range(1,101):
+        string = ""
+        if num % 3 == 0:
+            string = string + "Fizz"
+        if num % 5 == 0:
+            string = string + "Buzz"
+        if num % 3 != 0 and num % 5 != 0:
+            string = string + str(num)
+        print((string), end=" ")
+
+    print("$")
