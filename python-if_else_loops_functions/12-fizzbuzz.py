7ac27df3d50111e66cc8036c7c3a67c96dcda283
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-04 17:41:57 +0200

    add other method for fizzbuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
-#!/usr/bin/bash
-
+#!/usr/bin/python3
 def fizzbuzz():
-    for num in range(1, 101):
-        string = ""
-        if num % 3 == 0:
-            string += "Fizz"
-        if num % 5 == 0:
-            string += "Buzz"
-        if num % 3 != 0 and num % 5 != 0:
-            string += str(num)
-        if num != 101:
-            print("{}".format(string), end=" ")
+    for fizz in range(1, 101):
+        if (fizz % 3 == 0 and fizz % 5 == 0):
+            print("FizzBuzz", end=" ")
+        elif (fizz % 3 == 0):
+            print("Fizz", end=" ")
+        elif (fizz % 5 == 0):
+            print("Buzz", end=" ")
+        else:
+            print(fizz, end=" ")
