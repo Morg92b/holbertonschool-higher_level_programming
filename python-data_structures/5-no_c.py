f31d38a80de908ebec1e2a75d32eeaa288aa274e
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-09 15:12:37 +0200

    add string with no C or c

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""
+    for index in my_string:
+        if index != "c" and index != "C":
+            new_string += index
+    return new_string
