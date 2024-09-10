e0c97c7494da3afd181a1dbb833bcd6449bd7242
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-10 12:57:16 +0200

    fix because i need to keep the original dict

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] *= 2
-    return a_dictionary
+    return {key: value * 2 for key, value in a_dictionary.items()}
