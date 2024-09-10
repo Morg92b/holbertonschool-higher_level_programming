1041e65066c22bfb34ea59d0d0066f131a75e190
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-10 17:28:45 +0200

    fix pycodestyle

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[index]), end="")
             count += 1
         except (ValueError, TypeError):
-            continue  
+            continue
     print()
     return count
