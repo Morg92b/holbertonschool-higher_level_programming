5d61932d2892a9df951a4268c8677159b5abc0a5
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-12 17:32:05 +0200

    i try other method for fix error

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,9 +9,9 @@ def print_square(size):
     Return: print # with number size
     """
 
+    if isinstance(size, float) and size.is_integer():
+        size = int(size)
     if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
