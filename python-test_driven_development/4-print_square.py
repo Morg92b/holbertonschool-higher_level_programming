23e7c6e970e339474467be34aa9025f906add31a
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-12 17:23:21 +0200

    fix error code

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -11,6 +11,8 @@ def print_square(size):
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for index in range(size):
