9ac472766c52824f80b95fa5324d6232829110de
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-12 17:38:36 +0200

    i try new other method for vallid checker

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,11 +9,9 @@ def print_square(size):
     Return: print # with number size
     """
 
-    if isinstance(size, float) and size.is_integer():
-        size = int(size)
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    if size < 0:
+    if size < 0 or (isinstance(size, float) and size < 0):
         raise ValueError("size must be >= 0")
     for index in range(size):
         print("#" * size)
