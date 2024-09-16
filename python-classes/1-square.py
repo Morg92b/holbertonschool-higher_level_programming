c3e1fa5c47b17f5f0760a1f5cd3f87a100534c0a
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-16 14:27:42 +0200

    add square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Define a Square class
+"""
+
+
+class Square:
+    """
+    Class representing a square
+    """
+    def __init__(self, size):
+        """
+        Initialize a square with a given size
+        Args:
+            size (int): The size of the square
+        """
+        self.size = size
