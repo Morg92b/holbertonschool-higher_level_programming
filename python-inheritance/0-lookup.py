570212d4da0e888d62b45ecd561285709841d77b
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-23 11:05:39 +0200

    add Readme and add a function that returns the list with dir(obj)

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This function that returns the list of available attributes and methods of an object
+
+"""
+
+def lookup(obj):
+    """
+    Returns a list of attributes and methods of the given object
+    """
+    return dir(obj)
