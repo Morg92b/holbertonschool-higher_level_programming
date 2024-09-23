8ae21ad540535205a3c5926203a197f6ddc55496
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-23 15:02:09 +0200

    add geometry with a exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    A class with a method to calculate area
+    """
+    def area(self):
+        """
+        Raises an exception, indicating that area() is not implemented
+        """
+        raise Exception("area() is not implemented")
