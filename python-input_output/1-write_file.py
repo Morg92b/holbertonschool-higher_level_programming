c97915e0ff0447cb291491ca48b482926a41e9bf
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-30 18:21:38 +0200

    fix code with change print to return

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """This function that write file and print it to stdout"""
     with open(filename, "w", encoding="utf-8") as f:
-        print(f.write(text))
+        return f.write(text)
