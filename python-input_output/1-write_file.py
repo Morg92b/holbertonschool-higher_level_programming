0603471ae60a3a0fe8d4f0f2650d383dc4cc9c0d
Morg92b <morgan.bouaziz@outlook.fr>
2024-09-30 16:43:03 +0200

    fix pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """1. Write file """
 
+
 def write_file(filename="", text=""):
     """This function that write file and print it to stdout"""
     with open(filename, "w", encoding="utf-8") as f:
