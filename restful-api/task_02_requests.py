4da3eddd0d087e1a98b98294bff784fa1b611942
Morg92b <morgan.bouaziz@outlook.fr>
2024-10-07 17:30:56 +0200

    fix my print and pycodestyle

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,15 +3,17 @@
 import requests
 import csv
 
+
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print("Status Code: {}".format, response.status_code)
+    print("Status Code: {}".format(response.status_code))
 
     if response.status_code == 200:
         posts = response.json()
         for index in posts:
             print(index['title'])
+
 
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
@@ -24,4 +26,5 @@ def fetch_and_save_posts():
             fields = ['id', 'title', 'body']
             writer.writerow(fields)
             for index_2 in posts:
-                writer.writerow([index_2['id'], index_2['title'], index_2['body']])
+                writer.writerow([index_2['id'], index_2['title'],
+                                 index_2['body']])
