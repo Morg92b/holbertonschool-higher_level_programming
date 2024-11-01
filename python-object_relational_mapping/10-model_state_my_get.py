5f18e0bf01fa0e8e8b9552d7d37c50319a793b2d
Morg92b <morgan.bouaziz@outlook.fr>
2024-11-01 11:20:25 +0100

    add script that prints the State object with the name on argv

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+
+    """Connect to the database and get the state contain the letter a"""
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    instance = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if instance is None:
+        print("Not found")
+    else:
+        print("{}".format(instance.id))
+
+    session.close()
